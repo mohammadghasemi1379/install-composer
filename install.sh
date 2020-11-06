b6fdb5aa1cbeb1ef44a457d29790e002ed0a1acd
mohammad ghasemi <41265001+mohammadghasemi1379@users.noreply.github.com>
2020-11-06 22:45:24 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "installing..... \n";
+echo "\n";
+echo "\n";
+echo "\n";
+echo "\n";
+sudo apt install php php-zip unzip php-mbstring php-curl curl;
+sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');";
+sudo php -r "if (hash_file('sha384','composer-setup.php') === 'c31c1e292ad7be5f49291169c0ac8f683499edddcfd4e42232982d0f>sudo php composer-setup.php
+sudo php composer-setup.php --install-dir=/bin --filename=composer;
