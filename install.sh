d44aec9e4ca613112959b372e16e560458543b4c
mohammad ghasemi <41265001+mohammadghasemi1379@users.noreply.github.com>
2020-12-27 11:57:34 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 echo "installing....."; 
 sudo apt install php php-zip unzip php-mbstring php-curl curl;
 sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');";
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'c31c1e292ad7be5f49291169c0ac8f683499edddcfd4e42232982d0fd193004208a58ff6f353fde0012d35fdd72bc394') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php --install-dir=/bin --filename=composer;
